1c510274519b954725d969d5be19b8ab285f1083
yuuki <8425037@ed.tus.ac.jp>
2025-06-18 20:13:15 +0900

    chapter1

---
+++ index.ts
@@ -0,0 +1,5 @@
+import { addNumber } from './lib.js';
+import magicNumber from './lib.js'
+
+console.log(addNumber(1, 2)); // 3
+console.log(magicNumber); // 42
---
+++ lib.js
@@ -0,0 +1,7 @@
+export const addNumber = (a, b) => {
+  return a + b;
+};
+
+const magicNumber = 42;
+
+export default magicNumber;
